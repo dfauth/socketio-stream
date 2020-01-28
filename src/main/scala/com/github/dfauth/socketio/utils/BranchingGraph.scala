2c1b8d5f0b9b1d719462b00ebbb85feff8cfb9ed
damian <dfauth@gmail.com>
2020-01-28 17:16:16 +0800

    SplittingGraph

--- src/main/scala/com/github/dfauth/socketio/utils/BranchingGraph.scala
+++ src/main/scala/com/github/dfauth/socketio/utils/BranchingGraph.scala
@@ -1,6 +1,6 @@
 package com.github.dfauth.socketio.utils
 
-import akka.stream.{ActorMaterializer, ClosedShape, Materializer}
+import akka.stream.{ClosedShape, Materializer}
 import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Merge, RunnableGraph, Sink, Source}
 import com.github.dfauth.socketio.reactivestreams.{FunctionProcessor, PartialFunctionProcessor, Processors}
 import com.typesafe.scalalogging.LazyLogging
@@ -24,8 +24,8 @@ object SplittingGraph extends LazyLogging {
 
     val (sink0, src0) = Processors.sinkToSource[T]
     val (sink1, src1) = Processors.sinkToSource[T]
-    apply(src, sink0, sink1)
-    new IntermediateBuilderStage(src0, Seq(src1))
+    val graph:RunnableGraph[Any] = apply(src, sink0, sink1)
+    new IntermediateBuilderStage(graph, src0, Seq(src1))
 }
 
 def apply[T](src:Source[T, Any], sink:Sink[T, Any], sink2:Sink[T, Any]):RunnableGraph[Any] = RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
@@ -90,23 +90,29 @@ object ShortCircuit {
 }
 
 trait BuilderStage[T] {
+  protected val graph:RunnableGraph[Any]
   protected val src:Source[T, Any]
   def and(sink: Sink[T, Any]):BuilderStage[T] = to(sink)
-  def withSource:Source[T, Any] = src
+  def withSource:FinalBuilderStage[T] = new FinalBuilderStage[T](graph, src)
   def to(sink: Sink[T, Any]) = {
     src.to(sink)
     this
   }
 }
 
-class IntermediateBuilderStage[T](override val src:Source[T, Any], srcList:Seq[Source[T, Any]]) extends BuilderStage[T] {
+class IntermediateBuilderStage[T](override val graph:RunnableGraph[Any], override val src:Source[T, Any], srcList:Seq[Source[T, Any]]) extends BuilderStage[T] {
   override def to(sink: Sink[T, Any]):BuilderStage[T] = {
     src.to(sink)
     srcList match {
-      case src1 :: Nil => new FinalBuilderStage[T](src1)
-      case src1 :: tail => new IntermediateBuilderStage[T](src1, tail)
+      case src1 :: Nil => new FinalBuilderStage[T](graph, src1)
+      case src1 :: tail => new IntermediateBuilderStage[T](graph, src1, tail)
     }
   }
 }
 
-class FinalBuilderStage[T](override val src:Source[T, Any]) extends BuilderStage[T]
+class FinalBuilderStage[T](override val graph:RunnableGraph[Any], override val src:Source[T, Any]) extends BuilderStage[T] {
+  def run(implicit m:Materializer):Source[T, Any] = {
+    graph.run()
+    src
+  }
+}
