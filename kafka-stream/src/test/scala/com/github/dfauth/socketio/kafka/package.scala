5a3a7070874deb26a3abf72f7aa94a1180f5a5eb
damian <dfauth@gmail.com>
2020-02-02 08:02:09 +0800

    factor out duplicated static route

---
+++ kafka-stream/src/test/scala/com/github/dfauth/socketio/kafka/package.scala
@@ -0,0 +1,28 @@
+package com.github.dfauth.socketio
+
+import java.util.Properties
+
+import com.typesafe.scalalogging.LazyLogging
+import net.manub.embeddedkafka.EmbeddedKafkaConfig
+import org.apache.kafka.clients.producer.RecordMetadata
+
+import scala.util.{Failure, Success, Try}
+
+package object kafka extends LazyLogging {
+
+  def connectionProperties(config: EmbeddedKafkaConfig):Properties = {
+    val props = new Properties()
+    props.setProperty("zookeeperConnectString", s"localhost:${config.zooKeeperPort}")
+    props.setProperty("bootstrap.servers", s"localhost:${config.kafkaPort}")
+    props
+  }
+
+  val logSuccess: Try[RecordMetadata] => Unit = t => t match {
+    case Success(r) => {
+      logger.info(s"success: ${r}")
+    }
+    case Failure(f) => {
+      logger.error(f.getMessage, f)
+    }
+  }
+}
